1512eea7da380181986edd87aaf7b6a5de499195
guptarnav125 <arnav23125@iiitd.ac.in>
2025-09-06 00:36:14 +0530

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Name = (props) => {
   return <li>{props.name} {props.number}</li>
@@ -41,12 +42,7 @@ const Persons=({ persons }) => {
 }
 
 const App = () => {
-  const [allPersons, setAllPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [allPersons, setAllPersons] = useState([]) 
   const [persons, setPersons] = useState(allPersons);
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -89,6 +85,18 @@ const App = () => {
     setNewName('')
     setNewNumber('')
   }
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setAllPersons(response.data)
+        setPersons(response.data) 
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
 
   return (
