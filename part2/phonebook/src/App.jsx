c83bc91d2026b3f9e791355d6ecaa644702f7fb0
guptarnav125 <arnav23125@iiitd.ac.in>
2025-09-08 01:36:45 +0530

    2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,25 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './personService'
 
 const Name = (props) => {
-  return <li>{props.name} {props.number}</li>
+  const handleDelete = () => {
+    if (window.confirm(`Delete ${props.name} ?`)) {
+      personService
+        .deletePerson(props.id)
+        .then(() => {
+          alert(`${props.name} has been deleted`)
+          props.onDelete(props.id)
+        })
+        .catch(error => {
+          console.error("There was an error deleting the person!", error)
+        })
+    }
+  }
+  return <li>
+          {props.name} {props.number}  
+          <button onClick={handleDelete}> delete</button>
+        </li>
+    
 }
 
 const Filter = ({ filter, onFilterChange }) => {
@@ -31,11 +48,11 @@ const PersonForm=({ onSubmit, newName, onNameChange, newNumber, onNumberChange }
   )
 }
 
-const Persons=({ persons }) => {
+const Persons=({ persons, onDelete }) => {
   return (
     <ul>
       {persons.map((person,index) => 
-        <Name name={person.name} number={person.number} key={index}/>
+        <Name name={person.name} number={person.number} id ={person.id } onDelete={onDelete} key={person.id}/>
       )}
     </ul>
   )
@@ -50,12 +67,12 @@ const App = () => {
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
-    setNewName(event.target.value)
+    setNewName(event.target.value.trimStart())
   }
 
   const handleNumberChange = (event) => {
     console.log(event.target.value)
-    setNewNumber(event.target.value)
+    setNewNumber(event.target.value.trim())
   }
 
   const handleFilter = (event) => {
@@ -69,18 +86,54 @@ const App = () => {
     setPersons(filtered)
   }
 
+  const handleDelete = (id) => {
+    const updatedPersons = allPersons.filter(person => person.id !== id)
+    setAllPersons(updatedPersons)
+    setPersons(updatedPersons.filter(person =>
+      person.name.toLowerCase().includes(newFilter.toLowerCase())
+    ));
+  }
+
   const addName = (event) => {
     event.preventDefault()
-    const nameObject = { name: newName , number: newNumber, id: allPersons.length + 1 +""}
-    if (allPersons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-      setNewName('')
+    const trimmedName = newName.trim()
+    const trimmedNumber = newNumber.trim()
+    if (trimmedName === '' || trimmedNumber === '') {
+      alert("Name or Number cannot be empty")
       return
     }
-    axios
-      .post('http://localhost:3001/persons', nameObject)
-      .then(response => {
-        const updatedPersons = allPersons.concat(response.data)
+    const nameObject = { name: trimmedName, number: trimmedNumber}
+    if (allPersons.some(person => person.name === trimmedName && person.number === trimmedNumber)) {
+      alert(`${trimmedName} is already added to phonebook with this number`)
+      setNewName('')
+      setNewNumber('')
+      return
+    }
+    if (allPersons.some(person => person.name === trimmedName)) {
+      if (window.confirm(`${trimmedName} is already added to phone book, replace the old number with new one?`)) {
+        const id = allPersons.find(person => person.name === trimmedName).id
+        nameObject.id = id
+      personService
+        .update(id, nameObject)
+        .then(() => {
+          const updatedPersons = allPersons.map(person => person.name === trimmedName ? nameObject : person)
+          setAllPersons(updatedPersons)
+          setPersons(updatedPersons.filter(person =>
+            person.name.toLowerCase().includes(newFilter.toLowerCase())
+          ));
+        })
+        .catch(error => {
+          console.error("There was an error updating the person!", error)
+        })
+      }
+      setNewName('')
+      setNewNumber('')
+      return
+    }
+    personService
+      .create(nameObject)
+      .then(returnedPerson => {
+        const updatedPersons = allPersons.concat(returnedPerson)
         setAllPersons(updatedPersons)
         setPersons(updatedPersons.filter(person =>
         person.name.toLowerCase().includes(newFilter.toLowerCase())
@@ -92,12 +145,12 @@ const App = () => {
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
+    personService
+      .getAll()
+      .then(inititalPersons => {
         console.log('promise fulfilled')
-        setAllPersons(response.data)
-        setPersons(response.data) 
+        setAllPersons(inititalPersons)
+        setPersons(inititalPersons) 
       })
   }, [])
   console.log('render', persons.length, 'persons')
@@ -110,7 +163,7 @@ const App = () => {
       <h3>Add a new</h3>
       <PersonForm onSubmit={addName} newName={newName} onNameChange={handleNameChange} newNumber={newNumber} onNumberChange={handleNumberChange}/>
       <h3>Numbers</h3>
-      <Persons persons={persons}/>
+      <Persons persons={persons} onDelete={handleDelete}/>
     </div>
   )
 }
